48c6c0d9c0fd8c5e2b26068ffee25f0c6b2af21a
Robert Walery <robert.walery.99@gmail.com>
2022-03-29 10:56:50 +0200

    First layout wpf

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calendar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> tasks = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string aktualna_data = DateTime.Today.ToShortDateString();
+            string tmp = "Zadania do wykonania w dniu: " + aktualna_data;
+            label1.Content = tmp;
+        }
+
+
+        private void Create_task(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.ShowDialog();
+            tasks.Add(window1.new_task.Text);
+            list_of_tasks.ItemsSource = tasks;
+        }
+    }
+}
