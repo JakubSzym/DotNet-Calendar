cf0bec44329515ebe06cad27e784e6c390bb96fe
Robert Walery <robert.walery.99@gmail.com>
2022-04-21 19:09:58 +0200

    Doxygen 2

--- Item.cs
+++ Item.cs
@@ -2,9 +2,19 @@
 
 namespace Calendar
 {
+    /**
+    * @class Item Klasa służy do instancji wyświetlanych elementów.
+    **/
     public class Item
     {
+        /**
+        * @var Content Zawiera treść zadania.
+        **/
         public string Content { get; set; }
+
+        /**
+        * @var IsMarked Ta zmienna mówi nam czy element jest zaznaczony czy nie.
+        **/
         public bool IsMarked { get; set; }
 
         public Item(string content)
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,30 +16,11 @@ namespace Calendar
     /// 
     public partial class MainWindow : Window
     {
-        /**
-        * @var Itemms Przechowuje zadania do wykonania które są wyświetlane w oknie Aplikacji
-        **/
         List<Item> Items = new List<Item>();
-
-        /**
-        * @var Tasks Przechowuje zadania do wykonania pobrane z bazy danych
-        **/
         List<Task> Tasks = new List<Task>();
-
-        /**
-        * @var currentDate Przechowuje aktulną datę w postaci stringa.
-        **/
         string currentDate = DateTime.Today.ToShortDateString();
-
-        /**
-        * @var APIKey Przechowuje klucz do zewnętrzengo API - OpenWeather.
-        **/
         const string APIKey = "f2f160e83ec212a5ecbb5de99f90dbb5";
         
-        /**
-        * @brief Konstruktor klasy MainWindow. Inicjalizuje wszytskie obiektu XAML oraz uruchamia funkcję
-        * DisplayTasksAfterStart.
-        **/ 
         public MainWindow()
         {
             InitializeComponent();
@@ -47,10 +28,10 @@ namespace Calendar
             string buffer = "Things to do on: " + currentDate;
             label1.Content = buffer;
         }
-        
+
         /**
-        * @brief Funkcja wyświetla stworzone zadania na aktualny (dzisiejszy) dzień.
-        **/ 
+         * @brief 
+        **/
         public void DisplayTasksAfterStart()
         {
             using (var context = new ApplicationDbContext())
@@ -69,10 +50,6 @@ namespace Calendar
             ListOfTasks.Items.Refresh();
         }
 
-        /**
-        * @brief Funkcja odświeża kontrolke XAML (Liste), która wyświetla w oknie głownym 
-        * apliakcji nasze zadania do wykonania.
-        **/ 
         public void RefreshListOfTasks()
         {
             Items.Clear();
@@ -86,12 +63,6 @@ namespace Calendar
             ListOfTasks.Items.Refresh();
         }
 
-
-        /**
-        * @brief Funkcja ta tworzy nowe zadania i zapisuje je do bazy danych.
-        * @param sender Obiekt XAML który wywołuje tą funkcję.
-        * @param e Zdarzenie, które zostało wywołane na danej kontrolce.
-        **/ 
         private void CreateTask(object sender, RoutedEventArgs e)
         {
             Window1 window1 = new Window1();
@@ -109,13 +80,7 @@ namespace Calendar
 
             RefreshListOfTasks();
         }
-        
 
-        /**
-        * @brief Funkcja ta zmienia liste wyświetlanych zadań po wybraniu konkretnego dnia w kalendarzu.
-        * @param sender Obiekt XAML który wywołuje tą funkcję.
-        * @param e Zdarzenie, które zostało wywołane na danej kontrolce.
-        **/ 
         private void cal_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
             // ... See if a date is selected.
@@ -135,13 +100,6 @@ namespace Calendar
             RefreshListOfTasks();
         }
 
-
-        /**
-        * @brief Funkcja usuwa zaznaczone zadania do wykonania na konretny dzień. 
-        * Zadania do usnięcia są zaznaczone poprzez checkboxy
-        * @param sender Obiekt XAML który wywołuje tą funkcję.
-        * @param e Zdarzenie, które zostało wywołane na danej kontrolce.
-        **/ 
         private void DeleteMarkedTasks(object sender, RoutedEventArgs e)
         {
             foreach (var item in Items)
@@ -174,20 +132,11 @@ namespace Calendar
             RefreshListOfTasks();
         }
 
-        /**
-        * @brief Obsługuje kliknięcie przycisku SEARCH, w celu sprawdzenia pogody.
-        * @param sender Obiekt XAML który wywołuje tą funkcję.
-        * @param e Zdarzenie, które zostało wywołane na danej kontrolce.
-        **/ 
         private void ButtonSearchClick(object sender, RoutedEventArgs e)
         {
             GetWeather();
         }
 
-
-        /**
-        * @brief Funkcja ta pobiera informacje pogodowe z zewnętrznego API.
-        **/ 
         async void GetWeather()
         {
             using (HttpClient web = new HttpClient())
@@ -212,11 +161,6 @@ namespace Calendar
             }
         }
 
-        /**
-        * @brief Funkcja zamienia czas unixowy na datę i godzinę.
-        * @param seconds Liczba sekund od 1 stycznia 1970 roku.
-        * @return day Zwraca datę i godzinę.
-        **/ 
         DateTime GetTime(long seconds)
         {
             DateTime day = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
